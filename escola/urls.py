84979ea521fedfd77a522d3a18889db76c10c5d8
22407372RafaelMagalhaes <a22407372@alunos.ulht.pt>
2026-04-21 09:21:07 +0000

    Implementação da aba alunos, professores, curso

--- escola/urls.py
+++ escola/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('cursos/', views.cursos_view, name="cursos"),
     path('curso/<int:id>', views.curso_view, name="curso"),
+    path('alunos/', views.alunos_view, name='alunos'),
+    path('professores/', views.professores_view, name='professores'),
     path('', views.cursos_view),
 ]
