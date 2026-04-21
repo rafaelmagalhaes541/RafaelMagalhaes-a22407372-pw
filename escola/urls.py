b9e624f7d53198d19c6457391da366aff10e4d85
22407372RafaelMagalhaes <a22407372@alunos.ulht.pt>
2026-04-21 08:30:20 +0000

    Implementação do cursos.html

--- escola/urls.py
+++ escola/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('cursos/', views.cursos_view, name="cursos"),
+    path('curso/<int:id>', views.curso_view, name="curso"),
+    path('', views.cursos_view),
 ]
--- escola/views.py
+++ escola/views.py
@@ -6,3 +6,7 @@ from .models import Curso
 def cursos_view(request):
     cursos=Curso.objects.all()       
     return render(request, 'escola/cursos.html', {'cursos': cursos})
+
+def curso_view(request, id):
+    curso=Curso.objects.get(id=id)       
+    return render(request, 'escola/curso.html', {'curso': curso})
