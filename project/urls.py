13b42b8c2357fd603e4c64e1b52402276ebc5b34
22407372RafaelMagalhaes <a22407372@alunos.ulht.pt>
2026-04-25 21:49:52 +0000

    Pequenas alterações nos url patterns

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("escola/", include("escola.urls")),
-    path("", include("portfolio.urls")),   
+    path("portfolio/", include("portfolio.urls")),   
 ]
 
 # Serve ficheiros media em DEBUG
