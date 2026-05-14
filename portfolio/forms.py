a7f0340471a951267e69b0a16ea0fd62d848dfef
22407372RafaelMagalhaes <a22407372@alunos.ulht.pt>
2026-05-14 10:15:26 +0000

    Criação do form.html e views para editar, criar e apagar competencias, tecnologias e formacoes

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Projeto
+from .models import Projeto, Tecnologia, Competencia, Formacao
 
 class ProjetoForm(forms.ModelForm):
     class Meta:
         model = Projeto
         fields = ['titulo', 'descricao', 'data_de_realizacao', 'tecnologias', 'link']
+
+class TecnologiaForm(forms.ModelForm):
+    class Meta:
+        model = Tecnologia
+        fields = ['nome', 'tipo', 'descricao', 'site', 'interesse']
+
+class CompetenciaForm(forms.ModelForm):
+    class Meta:
+        model = Competencia
+        fields = ['nome', 'tipo', 'nivel', 'experiencia', 'projetos']
+
+class FormacaoForm(forms.ModelForm):
+    class Meta:
+        model = Formacao
+        fields = ['titulo', 'tipo', 'instituicao', 'data_inicio', 'data_conclusao', 'descricao', 'aluno']
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
-from .forms import ProjetoForm
+from .forms import ProjetoForm, TecnologiaForm, CompetenciaForm, FormacaoForm
 from .models import *
 
 
@@ -21,6 +21,45 @@ def competencia_view(request):
     )
     return render(request, 'portfolio/competencia.html', {'competencias': competencias})
 
+def competencia_create(request):
+    if request.method == 'POST':
+        form = CompetenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('competencia')
+    else:
+        form = CompetenciaForm()
+
+    return render(request, 'portfolio/form.html', {
+    'form': form,
+    'titulo': '➕ Nova Competência',
+    'subtitulo': 'Regista uma competência',
+    'voltar_url': '/portfolio/competencia/'
+    })
+
+def competencia_update(request, pk):
+    competencia = get_object_or_404(Competencia, pk=pk)
+
+    if request.method == 'POST':
+        form = CompetenciaForm(request.POST, instance=competencia)
+        if form.is_valid():
+            form.save()
+            return redirect('competencia')
+    else:
+        form = CompetenciaForm(instance=competencia)
+
+    return render(request, 'portfolio/form.html', {
+    'form': form,
+    'titulo': '✏️ Editar Competência',
+    'subtitulo': 'Atualiza a competência',
+    'voltar_url': '/portfolio/competencia/'
+    })
+
+def competencia_delete(request, pk):
+    competencia = get_object_or_404(Competencia, pk=pk)
+    competencia.delete()
+    return redirect('competencia')
+
 
 def formacao_view(request):
     formacoes = (
@@ -29,6 +68,45 @@ def formacao_view(request):
         .all()
     )
     return render(request, 'portfolio/formacao.html', {'formacoes': formacoes})
+
+def formacao_create(request):
+    if request.method == 'POST':
+        form = FormacaoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('formacao')
+    else:
+        form = FormacaoForm()
+
+    return render(request, 'portfolio/form.html', {
+    'form': form,
+    'titulo': '➕ Nova Formação',
+    'subtitulo': 'Adiciona formação académica',
+    'voltar_url': '/portfolio/formacao/'
+    })
+
+def formacao_update(request, pk):
+    formacao = get_object_or_404(Formacao, pk=pk)
+
+    if request.method == 'POST':
+        form = FormacaoForm(request.POST, instance=formacao)
+        if form.is_valid():
+            form.save()
+            return redirect('formacao')
+    else:
+        form = FormacaoForm(instance=formacao)
+
+    return render(request, 'portfolio/form.html', {
+        'form': form,
+        'titulo': '✏️ Editar Formação',
+        'subtitulo': 'Atualiza a formação selecionada',
+        'voltar_url': '/portfolio/formacao/'
+    })
+
+def formacao_delete(request, pk):
+    formacao = get_object_or_404(Formacao, pk=pk)
+    formacao.delete()
+    return redirect('formacao')
 
 
 def licenciatura_view(request):
@@ -67,7 +145,12 @@ def projeto_create(request):
     else:
         form = ProjetoForm()
 
-    return render(request, 'portfolio/projeto_form.html', {'form': form})
+    return render(request, 'portfolio/form.html', {
+        'form': form,
+        'titulo': '➕ Novo Projeto',
+        'subtitulo': 'Cria um novo projeto',
+        'voltar_url': '/portfolio/projeto/'
+    })
 
 def projeto_update(request, pk):
     projeto = get_object_or_404(Projeto, pk=pk)
@@ -80,7 +163,12 @@ def projeto_update(request, pk):
     else:
         form = ProjetoForm(instance=projeto)
 
-    return render(request, 'portfolio/projeto_form.html', {'form': form})
+    return render(request, 'portfolio/form.html', {
+        'form': form,
+        'titulo': '✏️ Editar Projeto',
+        'subtitulo': 'Atualiza os detalhes do projeto',
+        'voltar_url': '/portfolio/projeto/'
+    })
 
 def projeto_delete(request, pk):
     projeto = get_object_or_404(Projeto, pk=pk)
@@ -91,6 +179,45 @@ def projeto_delete(request, pk):
 def tecnologia_view(request):
     tecnologias = Tecnologia.objects.all()
     return render(request, 'portfolio/tecnologia.html', {'tecnologias': tecnologias})
+
+def tecnologia_create(request):
+    if request.method == 'POST':
+        form = TecnologiaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tecnologia')
+    else:
+        form = TecnologiaForm()
+
+    return render(request, 'portfolio/form.html', {
+        'form': form,
+        'titulo': '➕ Nova Tecnologia',
+        'subtitulo': 'Adiciona uma tecnologia ao portfólio',
+        'voltar_url': '/portfolio/tecnologia/'
+    })
+
+def tecnologia_update(request, pk):
+    tecnologia = get_object_or_404(Tecnologia, pk=pk)
+
+    if request.method == 'POST':
+        form = TecnologiaForm(request.POST, instance=tecnologia)
+        if form.is_valid():
+            form.save()
+            return redirect('tecnologia')
+    else:
+        form = TecnologiaForm(instance=tecnologia)
+
+    return render(request, 'portfolio/form.html', {
+        'form': form,
+        'titulo': '✏️ Editar Tecnologia',
+        'subtitulo': 'Altera os dados da tecnologia',
+        'voltar_url': '/portfolio/tecnologias/'
+    })
+
+def tecnologia_delete(request, pk):
+    tecnologia = get_object_or_404(Tecnologia, pk=pk)
+    tecnologia.delete()
+    return redirect('tecnologia')
 
 
 def tfc_view(request):
